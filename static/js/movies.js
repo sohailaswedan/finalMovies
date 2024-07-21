d42c321aec60d83d4e6864c93d721260a8f7e6ca
Sohaila Mohamed <sohailaswedan45@gmail.com>
2024-07-21 11:13:41 +0300

    js enhancement

--- static/js/movies.js
+++ static/js/movies.js
@@ -1,19 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const addLink = document.getElementById('addLink');
-    const updateLink = document.getElementById('updateLink');
-    const deleteLink = document.getElementById('deleteLink');
-
-    const userRole = sessionStorage.getItem('userRole');
-    if (userRole === 'admin') {
-        addLink.classList.add('d-block');
-        updateLink.classList.add('d-block');
-        deleteLink.classList.add('d-block');
-    } else {
-        addLink.classList.add('d-none');
-        updateLink.classList.add('d-none');
-        deleteLink.classList.add('d-none');
-    }
-});
 const userEmail = document.getElementById("email-displayed"); 
 let localEmail = localStorage.getItem("email"); 
 if (localEmail) {
